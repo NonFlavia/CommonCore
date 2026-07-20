94b2623398f6f303c999cb3d3443cb4a616e32b6
NonFlavia <assolutamentenonflavia@gmail.com>
2026-07-20 14:48:22 +0200

    Update ft_list_foreach.c

--- Exam00_Milestone02/level_3/ft_list_foreach.c
+++ Exam00_Milestone02/level_3/ft_list_foreach.c
@@ -1,11 +1,8 @@
-/*Write a function that takes a list and a function pointer, and applies this
-function to each element of the list.
-
+/*
 It must be declared as follows:
 
  void    ft_list_foreach(t_list *begin_list, void (*f)(void *));
 
-The function pointed to by f will be used as follows:
 
 (*f)(list_ptr->data);
 
