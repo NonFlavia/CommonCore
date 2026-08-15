f8eb16912b64311b4ac659083a93756a97249d96
NonFlavia <assolutamentenonflavia@gmail.com>
2026-08-15 19:41:24 +0200

    Update ft_atoi.c

--- Exam00_Milestone02/level_1/ft_atoi.c
+++ Exam00_Milestone02/level_1/ft_atoi.c
@@ -1,36 +1,26 @@
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-
 // hi, this code was made by flavides during the 42 Common Core!
 
-int ft_atoi(const char *s)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int res;
-    int sign;
-    i = 0;
-    res = 0;
-    sign = 1;
-    // remember to skip spacesssss
-    if (s[0] && (s[0] == '-' || s[0] == '+' ))
-    {
-        i = 1;
-        if (s[0] == '-')
-            sign = -1;
-    }
-    while (s[i])
-    {
-        if (s[i] >= '0' && s[i] <= '9')
-            res = (res * 10) + s[i] - '0';
-        i++;
-    }
-    return (res * sign);
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
 }
-/*
-int main()
-{
-    printf("%d num da stringa\n", ft_atoi("234"));
-    printf("%d num da stringa\n", atoi("234"));
-    return(0);
-}*/
