cbe7e1c687f29d8c0c6d468487022ccff07ff056
NonFlavia <assolutamentenonflavia@gmail.com>
2026-08-15 19:40:37 +0200

    Update alpha_mirror.c

--- Exam00_Milestone02/level_1/alpha_mirror.c
+++ Exam00_Milestone02/level_1/alpha_mirror.c
@@ -2,24 +2,26 @@
 
 // hi, this code was made by flavides during the 42 Common Core!
 
-void alpha_mirror(char *s)
+void	alpha_mirror(char *str)
 {
-    int i = 0;
-    while (s[i])
-    {
-        if (s[i] >= 'a' && s[i] <= 'z')
-            s[i] =  'z' + 'a' - s[i];
-        else if (s[i] >= 'a' && s[i] <= 'z')
-            s[i] =  'Z' + 'A' - s[i];
-        write(1, &s[i], 1);
-        i++;
-    }
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = 'z' + 'a' - str[i];
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = 'Z' + 'A' - str[i];
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
-int main(int ac, char **av)
+int	main(int argc, char **argv)
 {
-    if (ac == 2)
-        alpha_mirror(av[1]);
-    write(1, "\n", 1);
-    return (0);
+	if (argc == 2)
+		alpha_mirror(argv[1]);
+	write(1, "\n", 1);
+	return (0);
 }
