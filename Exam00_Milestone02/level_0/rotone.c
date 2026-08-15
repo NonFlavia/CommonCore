94a810ab0527c89400a29da07024dc95aaf16b38
NonFlavia <assolutamentenonflavia@gmail.com>
2026-08-15 18:46:04 +0200

    Create rotone.c

---
+++ Exam00_Milestone02/level_0/rotone.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+// hi, this code was made by flavides during the 42 Common Core!
+
+void	rotone(char *str)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			c = (str[i] - 'a' + 1) % 26 + 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			c = (str[i] - 'A' + 1) % 26 + 'A';
+		else
+			c = str[i];
+		write(1, &c, 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		rotone(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
