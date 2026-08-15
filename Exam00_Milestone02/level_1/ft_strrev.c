0bd4b74b2b013ad2908f2075bf926c9fb33d9730
NonFlavia <assolutamentenonflavia@gmail.com>
2026-08-15 20:00:17 +0200

    Create ft_strrev.c

---
+++ Exam00_Milestone02/level_1/ft_strrev.c
@@ -0,0 +1,23 @@
+// hi, this code was made by flavides during the 42 Common Core!
+
+char	*ft_strrev(char *str)
+{
+	int		i;
+	int		j;
+	char	tmp;
+
+	i = 0;
+	j = 0;
+	while (str[j])
+		j++;
+	j--;
+	while (i < j)
+	{
+		tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+		i++;
+		j--;
+	}
+	return (str);
+}
