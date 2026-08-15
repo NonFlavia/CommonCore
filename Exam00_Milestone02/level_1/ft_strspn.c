344d5c5d81d6e18dde20dbfd662296d629ecc647
NonFlavia <assolutamentenonflavia@gmail.com>
2026-08-15 20:00:56 +0200

    Create ft_strspn.c

---
+++ Exam00_Milestone02/level_1/ft_strspn.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+
+// hi, this code was made by flavides during the 42 Common Core!
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j] && s[i] != accept[j])
+			j++;
+		if (!accept[j])
+			return (i);
+		i++;
+	}
+	return (i);
+}
