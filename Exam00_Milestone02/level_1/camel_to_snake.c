536f15aab993be924090b7feb353b4aab8b0ff6e
NonFlavia <assolutamentenonflavia@gmail.com>
2026-08-15 19:35:29 +0200

    Create camel_to_snake.c

---
+++ Exam00_Milestone02/level_1/camel_to_snake.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+
+// hi, this code was made by flavides during the 42 Common Core!
+
+void camel_to_snake(char *s)
+{
+    int i = 0;
+    while (s[i])
+    {
+        if (s[i] >= 'A' && s[i] <= 'Z')
+        {
+            write(1, "_", 1);
+            s[i] += 32;
+        }
+        write(1, &s[i], 1);
+        i++;
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2)
+        camel_to_snake(av[1]);
+    write(1, "\n", 1);
+}
